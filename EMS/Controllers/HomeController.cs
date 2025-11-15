6d1f2d83ea31e5270df2be6a9be81acd80ac2f7e
samiulsani <samiulslm5@gmail.com>
2025-11-16 01:00:29 +0600

    Index page About page create and design

--- EMS/Controllers/HomeController.cs
+++ EMS/Controllers/HomeController.cs
@@ -47,6 +47,11 @@ namespace EMS.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
